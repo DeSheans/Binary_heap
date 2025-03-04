3b71fca035af35b351bcfebba9ffa0f623bcfd81
DeSheans <desheans@yandex.ru>
2025-03-04 17:27:55 +0300

    Changed the constraints for the heap parameter to the any, added compare function in struct, added constructor with func param.

--- heap/heap.go
+++ heap/heap.go
@@ -3,33 +3,46 @@ package heap
 import "cmp"
 
 // Heap is a data structure for working with a min binary heap.
-type Heap[T cmp.Ordered] struct {
-	heap []T
+type Heap[T any] struct {
+	heap    []T
+	compare func(x, y T) int
 }
 
 // NewHeap creates and returns a new Heap instance initialized with the given slice.
 func NewHeap[T cmp.Ordered](s []T) *Heap[T] {
-	buildHeap(s)
-	return &Heap[T]{
-		heap: s,
+	h := &Heap[T]{
+		heap:    s,
+		compare: cmp.Compare[T],
 	}
+	buildHeap(h)
+	return h
+}
+
+// NewHeapWithComparer ...
+func NewHeapWithComparer[T any](s []T, comparer func(x, y T) int) *Heap[T] {
+	h := &Heap[T]{
+		heap:    s,
+		compare: comparer,
+	}
+	buildHeap(h)
+	return h
 }
 
 // Insert adds a new element to the heap.
 func (h *Heap[T]) Insert(val T) {
 	h.heap = append(h.heap, val)
-	siftUp(h.heap, len(h.heap)-1)
+	h.siftUp(len(h.heap) - 1)
 }
 
 // Remove deletes the specified value from the heap if it exists. Returns true if the value was found and removed, otherwise false.
 func (h *Heap[T]) Remove(val T) bool {
 	for i, v := range h.heap {
-		if v == val {
+		if h.compare(v, val) == 0 {
 			size := len(h.heap) - 1
 			h.heap[i], h.heap[size] = h.heap[size], h.heap[i]
 			size--
 			h.heap = h.heap[:size]
-			heapify(h.heap, i, size)
+			h.heapify(i, size)
 			return true
 		}
 	}
@@ -46,7 +59,7 @@ func (h *Heap[T]) Pop() T {
 	h.heap[0], h.heap[size] = h.heap[size], result
 	h.heap = h.heap[:size]
 	size--
-	heapify(h.heap, 0, size)
+	h.heapify(0, size)
 	return result
 }
 
@@ -66,38 +79,39 @@ func (h *Heap[T]) Sort() []T {
 		result[i] = h.heap[0]
 		h.heap[0], h.heap[size] = h.heap[size], h.heap[0]
 		size--
-		heapify(h.heap, 0, size)
+		h.heapify(0, size)
 	}
-	buildHeap(h.heap)
+	buildHeap(h)
 	return result
 }
 
-func buildHeap[T cmp.Ordered](s []T) {
-	size := len(s) - 1
-	for i := (len(s) >> 1) - 1; i >= 0; i-- {
-		heapify(s, i, size)
+func buildHeap[T any](heap *Heap[T]) {
+	size := len(heap.heap) - 1
+
+	for i := (len(heap.heap) >> 1) - 1; i >= 0; i-- {
+		heap.heapify(i, size)
 	}
 }
 
-func heapify[T cmp.Ordered](heap []T, index int, size int) {
+func (h *Heap[T]) heapify(index int, size int) {
 	leftInd := (index << 1) + 1
 	rightInd := leftInd + 1
 	minInd := index
-	if leftInd <= size && heap[leftInd] < heap[minInd] {
+	if leftInd <= size && h.compare(h.heap[leftInd], h.heap[minInd]) == -1 {
 		minInd = leftInd
 	}
-	if rightInd <= size && heap[rightInd] < heap[minInd] {
+	if rightInd <= size && h.compare(h.heap[rightInd], h.heap[minInd]) == -1 {
 		minInd = rightInd
 	}
 	if minInd != index {
-		heap[index], heap[minInd] = heap[minInd], heap[index]
-		heapify(heap, minInd, size)
+		h.heap[index], h.heap[minInd] = h.heap[minInd], h.heap[index]
+		h.heapify(minInd, size)
 	}
 }
 
-func siftUp[T cmp.Ordered](heap []T, ind int) {
-	for ind != 0 && heap[ind] < heap[(ind-1)>>1] {
-		heap[ind], heap[(ind-1)>>1] = heap[(ind-1)>>1], heap[ind]
+func (h *Heap[T]) siftUp(ind int) {
+	for ind != 0 && h.compare(h.heap[ind], h.heap[(ind-1)>>1]) == -1 {
+		h.heap[ind], h.heap[(ind-1)>>1] = h.heap[(ind-1)>>1], h.heap[ind]
 		ind = (ind - 1) >> 1
 	}
 }
